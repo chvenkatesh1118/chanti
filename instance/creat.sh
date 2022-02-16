d16269cb19fc667b051b46b35353a3bb2a8951ea
Chanti <chvenkatesh.1118@gmail.com>
2022-02-16 19:39:40 +0530

    11-02

---
+++ instance/creat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf .terraform
+terraform init
+terraform apply -auto-approve
---
+++ instance/ec2.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "sg1" {
+  name        = "sg1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0bf57a4f41c708816"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sg1"
+  }
+}
