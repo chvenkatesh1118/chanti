bf8438a7061da4129f2e85a6bc7066ee9df918e4
Chanti <chvenkatesh.1118@gmail.com>
2022-02-17 09:51:02 +0530

    11-02

--- instance/backend.tf
+++ instance/backend.tf
@@ -1,12 +1,12 @@
 terraform {
-backend "s3"  {
- bucket =  "chanti.ch"
- key = "terraform/terraform.tfstate"
- region = "us-east-1"
+ backend "s3"  {
+  bucket =  "chanti.ch"
+  key = "terraform/terraform.tfstate"
+  region = "us-east-1"
 
-  }
+ }
 }
 
 provider "aws" {
-  region = "us-east-1"
+ region = "us-east-1"
 }
