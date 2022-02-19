f21d09f27235fdec648fd942f2e8fcaa7c1828ea
Chanti <chvenkatesh.1118@gmail.com>
2022-02-19 22:46:24 +0530

    11-02

--- instance/creat.sh
+++ instance/creat.sh
@@ -8,7 +8,6 @@
 
  #!/bin/bash
 
-aws ec2 terminate-instances --image-id ami-0d997c5f64a74852c --count 1 --instance-type t2.micro \
- --subnet-id subnet-0f42b250c3cf1d75c  --instance-ids i-0342d07d1f361a248
---security-group-ids sg-03439c9546a230549 \
+aws ec2 terminate-instances  \
+  --instance-ids i-0342d07d1f361a248  \
 --user-data file://creat.sh
