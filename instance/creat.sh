7784c3bd9ed6bdf3da5fb55cb95a4ec278ac556c
Chanti <chvenkatesh.1118@gmail.com>
2022-02-19 22:37:46 +0530

    11-02

--- instance/creat.sh
+++ instance/creat.sh
@@ -9,5 +9,5 @@
  #!/bin/bash
 
 aws ec2 run-instances --image-id ami-0d997c5f64a74852c --count 1 --instance-type t2.micro \
---key-name no-key-pair --subnet-id subnet-0f42b250c3cf1d75c --security-group-ids sg-03439c9546a230549 \
+ --subnet-id subnet-0f42b250c3cf1d75c --security-group-ids sg-03439c9546a230549 \
 --user-data file://creat.sh
