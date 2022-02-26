5e2fb8ab17682dd4ac9b01e111e0355a7294ede2
Chanti <chvenkatesh.1118@gmail.com>
2022-02-26 22:25:02 +0530

    11-02

--- components/ec2.tf
+++ components/ec2.tf
@@ -13,35 +13,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "all2" {
-  name        = "all2"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "vpc-0bf57a4f41c708816"
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 1
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = [ "0.0.0.0/0" ]
-
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "all2"
-  }
-}
-
-
-
-output "all2" {
-  value = aws_security_group.all2.id
-}
