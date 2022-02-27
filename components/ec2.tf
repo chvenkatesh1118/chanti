46154e879eba77a1732d028cc3be178504571421
Chanti <chvenkatesh.1118@gmail.com>
2022-02-27 06:59:08 +0530

    11-02

--- components/ec2.tf
+++ components/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami           = "ami-0d997c5f64a74852c"
   instance_type = "t2.micro"
   subnet_id = "subnet-0f42b250c3cf1d75c"
-  vpc_security_group_ids = "sg-03439c9546a230549"
+ security_groups = "sg-03439c9546a230549"
 
   tags = {
     Name = var.NAME
