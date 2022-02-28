11ee79087f81ca4f8ef4f02896b2bfe4ec018783
Chanti <chvenkatesh.1118@gmail.com>
2022-02-28 17:48:35 +0530

    11-02

--- components/frontend/ec2.tf
+++ components/frontend/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance"  {
   ami            = "ami-0d997c5f64a74852c"
   instance_type  = "t2.micro"
   subnet_id      = "subnet-05149ab4e1f58d6b4"
-  security_groups = ["default"]
+
 
 
 
