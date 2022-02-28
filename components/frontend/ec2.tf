347d2415d9ca059b5fdc71ffb9f51af2caea45fa
Chanti <chvenkatesh.1118@gmail.com>
2022-02-28 17:55:53 +0530

    11-02

--- components/frontend/ec2.tf
+++ components/frontend/ec2.tf
@@ -1,20 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "instance"  {
   ami            = "ami-0d997c5f64a74852c"
   instance_type  = "t2.micro"
   subnet_id      = "subnet-05149ab4e1f58d6b4"
-
-
-
-
 
   tags = {
     Name = "frontend"
   }
 }
 
-
-
-provider "aws" {
-  region = "us-east-1"
+output "ip" {
+  value = aws_instance.instance.private_ip
 }
+
 
