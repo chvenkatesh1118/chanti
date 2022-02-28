3da386d9bbf4065283aab788f7f09bf03844f7bc
Chanti <chvenkatesh.1118@gmail.com>
2022-02-28 17:46:26 +0530

    11-02

--- components/frontend/ec2.tf
+++ components/frontend/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "instance"  {
   ami            = "ami-0d997c5f64a74852c"
   instance_type  = "t2.micro"
-  subnet_id      = "subnet-0f42b250c3cf1d75c"
-  security_groups = ["roboshop"]
+  subnet_id      = "subnet-05149ab4e1f58d6b4"
+  security_groups = ["default"]
 
 
 
